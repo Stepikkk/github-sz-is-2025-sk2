9c69cb65db806b47aaac1aba0d6da7908bc8deff
Štěpík <stepan.zavacky@tul.cz>
2025-10-02 13:11:45 +0200

    Prvni programek

---
+++ IS-Programy/prvni_program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej první hodnotu (first): ");
+        int first = int.Parse(Console.ReadLine());
+
+        Console.Write("Zadej poslední hodnotu (last): ");
+        int last = int.Parse(Console.ReadLine());
+
+        Console.Write("Zadej krok (step): ");
+        int step = int.Parse(Console.ReadLine());
+
+        for (int i = first; i < last; i += step)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
