71a9fed69df1e39b9edb68f80961e95ea50d7729
Štěpík <92027537+Stepikkk@users.noreply.github.com>
2025-10-13 18:03:53 +0200

    Create Program.cs

---
+++ IS-Programy/program000a-zakladni-kod/Program.cs
@@ -0,0 +1,48 @@
+﻿﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Výpis řady čísel *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Štěpán Závacký ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu - špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup hodnoty do programu - řešený správně
+    int first;
+    int last;
+    int step;
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+    Console.Write("Zadejte poslední číslo řady (celé číslo): ");
+
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+    }
+    Console.Write("Zadejte step řady (celé číslo): ");
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte step znovu: ");
+    }
+
+    
+    for (int i = first; i < last; i += step)
+        {
+            Console.WriteLine(i);
+        }
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a.");
+    again = Console.ReadLine();
+
+
+}
