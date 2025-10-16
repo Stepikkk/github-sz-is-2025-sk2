aafde084fa79fe9c68671e85bed777e8a7758438
Štěpík <stepan.zavacky@tul.cz>
2025-10-16 13:57:35 +0200

    Ciferny soucet

---
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.Numerics;
+    Console.Clear();
+    Console.WriteLine("****************************");
+    Console.WriteLine("***** Ciferný součet *****");
+    Console.WriteLine("****************************");
+    Console.WriteLine("******* Štěpán Závacký ********");
+    Console.WriteLine("****************************");
+    Console.WriteLine();
+
+int cislo;
+int soucet = 0;
+Console.WriteLine("Zadej celé číslo, pro který počítáme ciferný součet.");
+while (!int.TryParse(Console.ReadLine(), out cislo))
+{
+    Console.WriteLine("Zadaná hodnota není celé číslo.");
+}
+
+if (cislo < 0) cislo = -cislo;
+
+foreach(char cislice in cislo.ToString())
+{
+    soucet += cislice - '0';
+    Console.WriteLine("Přičítám číslo {0}. Aktuální hodnota: {1}", cislice, soucet);
+}
